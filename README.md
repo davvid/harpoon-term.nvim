fc6ecfe22dd9b7b66100448f9d96a1f2eea84de3
David Aguilar <davvid@gmail.com>
2024-12-26 15:18:42 -0800

    harpoon-term: add support for floating windows
    Terminals can now be opened in floating windows.
    
    toggle_floating_window(idx) can be used to toggle a floating window
    containing a terminal.
    
    The goto_terminal(idx) and send_command(idx, cmd) functions will operate
    on floating windows if a terminal is open using a floating window.
    
    The dedicated send_command_to_floating_window(idx, cmd) function can be
    used to ensure that the command is sent to a floating window that is
    toggled open when the command is sent.

--- README.md
+++ README.md
@@ -2,30 +2,90 @@
 
 Quickly jump to terminals.
 
-*Harpoon Term* is a friendly fork of the `harpoon.term` module from
-[harpoon](https://github.com/ThePrimeagent/harpoon).
+*Harpoon Term* is a standalone version of the `harpoon.term` module from
+[Harpoon](https://github.com/ThePrimeagent/harpoon) v1 that has been enhanced
+with support for floating windows.
 
-`harpoon.term` was removed in Harpoon v2 (in the `harpoon2` branch).
+The `harpoon.term` module was removed in Harpoon v2 (Cf. the `harpoon2` branch).
 
-This plugin is a minimalist copy of the core features from `harpoon.term`
-for use alongside `harpoon2` or standalone.
+`harpoon-term` provides the functionality from the legacy `harpoon.term` module in a
+standalone plugin.
+
+
 
 ## Installation
 
 You can install this plugin using your favorite vim package manager, eg.
-[vim-plug](https://github.com/junegunn/vim-plug),
-[Packer](https://github.com/wbthomason/packer.nvim) or
-[lazy](https://github.com/folke/lazy.nvim).
-
-**Packer**:
-```lua
-use({'davvid/harpoon-term.nvim'})
-```
+[lazy](https://github.com/folke/lazy.nvim),
+[vim-plug](https://github.com/junegunn/vim-plug) or
+[Packer](https://github.com/wbthomason/packer.nvim).
 
 **lazy**:
 ```lua
 {
-    'davvid/harpoon-term.nvim'
+    'davvid/harpoon-term.nvim',
+    keys = function()
+        local harpoon_term = require('harpoon_term')
+        return {
+            {
+                '<leader>tt',
+                function()
+                    harpoon_term.toggle_floating_window(nil)
+                end,
+                desc = 'HarpoonTerm Toggle Floating Window with Current Terminal',
+            },
+            {
+                '<leader>t1',
+                function()
+                    harpoon_term.toggle_floating_window(1)
+                end,
+                desc = 'HarpoonTerm Toggle Floating Window with Terminal #1',
+            },
+            {
+                '<leader>t2',
+                function()
+                    harpoon_term.toggle_floating_window(2)
+                end,
+                desc = 'HarpoonTerm Toggle Floating Window with Terminal #2',
+            },
+            {
+                '<leader>tc',
+                function()
+                    local idx = vim.fn.line('.')
+                    local cmd = vim.api.nvim_buf_get_lines(0, idx - 1, idx, false)[1]
+                    if cmd then
+                        harpoon_term.send_command_to_floating_window(nil, cmd)
+                    end
+                end,
+                desc = 'HarpoonTerm Send Line to Current Terminal in a Floating Window',
+            },
+            {
+                '<leader><leader>c1',
+                function()
+                    local idx = vim.fn.line('.')
+                    local cmd = vim.api.nvim_buf_get_lines(0, idx - 1, idx, false)[1]
+                    if cmd then
+                        harpoon_term.send_command_to_floating_window(1, cmd)
+                    end
+                end,
+                desc = 'HarpoonTerm Send Line to Terminal #1 in a Floating Window',
+            },
+            {
+                '<leader><leader>1',
+                function()
+                    harpoon_term.goto_terminal(1)
+                end,
+                desc = 'HarpoonTerm Switch to Terminal #1',
+            },
+            {
+                '<leader><leader>2',
+                function()
+                    harpoon_term.goto_terminal(2)
+                end,
+                desc = 'HarpoonTerm Switch to Terminal #2',
+            },
+        }
+    end,
 }
 ```
 
@@ -34,25 +94,74 @@ use({'davvid/harpoon-term.nvim'})
 Plug 'davvid/harpoon-term.nvim'
 ```
 
+**Packer**:
+```lua
+use({'davvid/harpoon-term.nvim'})
+```
+
+
 ## Usage
 
-To bind `goto_terminal(1)` to `<leader><leader>1` and send the current line to terminal 1
-using `<leader><leader>c1` use:
+Terminals are identified by "Termainl IDs" which are integers starting at `1`.
+Specify `nil` as the Terminal ID to automatically use the most recently used terminal.
+
+The following example keybindings demonstrate how to use this plugin.
+
+To bind `<leader><leader>1` so that it opens Terminal #1 use the following snippet in
+your `init.lua`:
 
 ```lua
+-- Switch to Terminal #1.
 vim.keymap.set({'n', 'v'}, '<leader><leader>1', function()
     require('harpoon_term').goto_terminal(1)
 end)
+```
 
+The `goto_terminal(idx)` function switches the current buffer to the specified terminal ID.
+
+To bind `<leader><leader>c1` so that it sends the current line to Terminal #1 use the
+following snippet in your `init.lua`:
+
+```lua
+-- Send the current line to Terminal #1.
 vim.keymap.set('n', '<leader><leader>c1', function()
     local idx = vim.fn.line('.')
     local cmd = vim.api.nvim_buf_get_lines(0, idx - 1, idx, false)[1]
-    if cmd == nil then
-        return nil
+    if cmd then
+        require('harpoon_term').send_command(1, cmd)
     end
-    require('harpoon_term').send_command(1, cmd)
 end)
 ```
+
+As mentioned above, replace the the number `1` with `nil` in the `goto_terminal(...)`
+and `send_command(...)` calls to automatically use the most recently used terminal.
+
+Floating windows can be used to open terminal instead of using the current buffer.
+
+```lua
+-- Toggle a Floating Window with the Current Terminal.
+vim.keymap.set({'n', 'v'}, '<leader>tt', function()
+    require('harpoon_term').toggle_floating_window(nil)
+end)
+
+-- Toggle a Floating Window with Terminal #1.
+vim.keymap.set({'n', 'v'}, '<leader>t1', function()
+    require('harpoon_term').toggle_floating_window(1)
+end)
+
+-- Send the current line to the most recently used Terminal in a floating window.
+vim.keymap.set('n', '<leader>tc', function()
+    local idx = vim.fn.line('.')
+    local cmd = vim.api.nvim_buf_get_lines(0, idx - 1, idx, false)[1]
+    if cmd then
+        require('harpoon_term').send_command_to_floating_window(nil, cmd)
+    end
+end)
+```
+
+Press `<ESC>` or `<Ctrl-c>` in Normal Mode when the floating window is open to close the
+window.
+
 
 ## Development
 
--- lua/harpoon_term/init.lua
+++ lua/harpoon_term/init.lua
@@ -1,17 +1,36 @@
 local M = {}
 
-local global_config = {
+local config = {
     cmds = {},
     enter_on_sendcmd = true,
-    save_on_change = true,
+    border = 'rounded',
+    title = 'Terminal',
+    title_pos = 'center',
+    style = 'minimal',
+    ui_width_ratio = 0.8,
+    ui_height_ratio = 0.8,
+    ui_fallback_width = 88,
+    ui_fallback_height = 42,
+    ui_max_width = nil,
+    ui_max_height = nil,
 }
+-- Internal global state
+M.harpoon_term_augroup = vim.api.nvim_create_augroup('HarpoonTerm', {})
+M.config = config
+M.current_win_id = nil
+M.current_buf_id = nil
+M.current_terminal = nil
 
 local terminals = {}
 
-function M.setup(config)
-    for key, value in pairs(config) do
-        global_config[key] = value
+function M.setup(opts)
+    for key, value in pairs(opts) do
+        M.config[key] = value
     end
+end
+
+function M.get_config(opts)
+    return vim.tbl_extend('force', M.config, opts or {})
 end
 
 function M.create_terminal(create_with)
@@ -51,6 +70,7 @@ function M.find_terminal(args)
         term_handle = {
             buf_id = buf_id,
             term_id = term_id,
+            floating_window = false,
         }
         terminals[args.idx] = term_handle
     end
@@ -58,7 +78,7 @@ function M.find_terminal(args)
 end
 
 local function get_first_empty_slot()
-    for idx, cmd in pairs(global_config.cmds) do
+    for idx, cmd in pairs(M.config.cmds) do
         if cmd == '' then
             return idx
         end
@@ -70,21 +90,41 @@ function M.goto_terminal(idx)
     local term_handle = M.find_terminal(idx)
     if term_handle ~= nil then
         vim.api.nvim_set_current_buf(term_handle.buf_id)
+        M.current_buf_id = term_handle.buf_id
+        M.current_terminal = idx
+        if M.current_win_id then
+            term_handle.floating_window = true
+        end
     end
 end
 
 function M.send_command(idx, cmd, ...)
+    M._send_command(false, idx, cmd, ...)
+end
+
+function M.send_command_to_floating_window(idx, cmd, ...)
+    M._send_command(true, idx, cmd, ...)
+end
+
+function M._send_command(float, idx, cmd, ...)
+    if idx == nil then
+        idx = M.get_current_terminal()
+    end
     local term_handle = M.find_terminal(idx)
     if term_handle == nil then
         return
     end
 
     if type(cmd) == 'number' then
-        cmd = global_config.cmds[cmd]
+        cmd = M.config.cmds[cmd]
     end
 
-    if global_config.enter_on_sendcmd then
+    if M.config.enter_on_sendcmd then
         cmd = cmd .. '\n'
+    end
+
+    if (float or term_handle.floating_window) and M.current_win_id == nil then
+        M.toggle_floating_window(idx)
     end
 
     if cmd then
@@ -100,7 +140,7 @@ function M.clear_all()
 end
 
 function M.get_length()
-    return table.maxn(global_config.cmds)
+    return table.maxn(M.config.cmds)
 end
 
 function M.valid_index(idx)
@@ -114,18 +154,111 @@ function M.emit_changed() end
 
 function M.add_command(cmd)
     local found_idx = get_first_empty_slot()
-    global_config.cmds[found_idx] = cmd
+    M.config.cmds[found_idx] = cmd
     M.emit_changed()
 end
 
 function M.set_command_list(new_list)
-    for k in pairs(global_config.cmds) do
-        global_config.cmds[k] = nil
+    for k in pairs(M.config.cmds) do
+        M.config.cmds[k] = nil
     end
     for k, v in pairs(new_list) do
-        global_config.cmds[k] = v
+        M.config.cmds[k] = v
     end
     M.emit_changed()
+end
+
+function M.get_floating_window_size(opts)
+    local width = opts.ui_fallback_width
+    local height = opts.ui_fallback_height
+    local win = vim.api.nvim_list_uis()
+    if #win > 0 then
+        width = math.floor(win[1].width * opts.ui_width_ratio)
+        height = math.floor(win[1].height * opts.ui_height_ratio)
+    end
+    if opts.ui_max_width and width > opts.ui_max_width then
+        width = opts.ui_max_width
+    end
+    if opts.ui_max_height and height > opts.ui_max_height then
+        height = opts.ui_max_height
+    end
+
+    return width, height
+end
+
+function M._create_window(buf_id, opts)
+    local width, height = M.get_floating_window_size(opts)
+    local win_id = vim.api.nvim_open_win(buf_id, true, {
+        relative = 'editor',
+        title = opts.title,
+        title_pos = opts.title_pos,
+        row = math.floor(((vim.o.lines - height) / 2) - 1),
+        col = math.floor((vim.o.columns - width) / 2),
+        width = width,
+        height = height,
+        style = opts.style,
+        border = opts.border,
+    })
+    return win_id
+end
+
+function M.toggle_floating_window(idx, opts)
+    if M.current_win_id and M.close_floating_window() then
+        return
+    end
+    opts = M.get_config(opts)
+    if idx == nil then
+        idx = M.get_current_terminal()
+    end
+    local term_handle = M.find_terminal(idx)
+    if term_handle == nil then
+        return
+    end
+
+    local win_id = M._create_window(term_handle.buf_id, opts)
+    if win_id ~= 0 then
+        term_handle.floating_window = true
+        M.current_win_id = win_id
+        M.current_buf_id = term_handle.buf_id
+        M.current_terminal = idx
+
+        -- Close the window when ESC or Ctrl-C is pressed.
+        vim.keymap.set('n', '<ESC>', function()
+            M.close_floating_window()
+        end, { buffer = term_handle.buf_id, silent = true })
+
+        vim.keymap.set('n', '<C-c>', function()
+            M.close_floating_window()
+        end, { buffer = term_handle.buf_id, silent = true })
+
+        -- Clear state when the terminal exits (e.g. when typing "exit").
+        vim.api.nvim_create_autocmd({ 'TermClose' }, {
+            group = M.harpoon_term_augroup,
+            buffer = term_handle.buf_id,
+            callback = function()
+                M.close_floating_window()
+                term_handle.floating_window = false
+            end,
+        })
+    end
+end
+
+function M.get_current_terminal()
+    if M.current_terminal == nil then
+        M.current_terminal = 1
+    end
+    return M.current_terminal
+end
+
+function M.close_floating_window()
+    local closed = false
+    if M.current_win_id and vim.api.nvim_win_is_valid(M.current_win_id) then
+        vim.api.nvim_win_close(M.current_win_id, true)
+        closed = true
+    end
+    M.current_win_id = nil
+
+    return closed
 end
 
 return M
